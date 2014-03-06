bae65602107834006bb948e4208aef0c33065beb
Patrick Moore <moore267@marshall.edu>
2014-03-06 09:41:01 -0500

    Added node attribute per Eric's suggestion. Added unregister recipe until an unregister action is added. Works in windows and linux

--- providers/default.rb
+++ providers/default.rb
@@ -1,28 +1,32 @@
 action :register do
-#  not_if exists?
-    Chef::Log.warn("Registering node with zen server #{new_resource}")
-#    ruby_block "register" do
-#      block do
+  Chef::Log.info("Current Zenoss Client Registration Status: #{node['zenoss-client']['registered']}")
+  if node['zenoss-client']['registered'] != true
+
+    Chef::Log.info("Registering node with zen server #{new_resource}")
+
         require 'net/http'
         base_uri = "#{new_resource.baseuri}"
         post_uri = "?deviceName=#{new_resource.hostname}&devicePath=#{new_resource.devicePath}&productionState=#{new_resource.productionValue}&loadDevice:method=1"
         full_uri = URI(base_uri + post_uri)
 
-	Chef::Log.warn(base_uri + post_uri)
+	Chef::Log.info("POST URI: " + base_uri + post_uri)
 
-        #req = Net::HTTP::Post.new(full_uri.path + "?" + full_uri.query)
-        #req = Net::HTTP::Post.new(full_uri.to_s)
         req = Net::HTTP::Post.new(base_uri + post_uri)
         req.basic_auth "#{new_resource.username}", "#{new_resource.password}"
 
         res = Net::HTTP.start(full_uri.hostname, full_uri.port).request(req)
-        puts res.body
-	Chef::Log.warn(res.code)
-#      end  
-#    end
-#    new_resource.updated_by_last_action(true)
-end
+        output = res.body
 
-def exists?
-  ::File.exists?("#{Chef::Config[:file_cache_path]}/zenrun.txt")
+	Chef::Log.info("HTTP return code: " + res.code)
+
+	if output.include? "Device #{new_resource.hostname} loaded!"
+	  Chef::Log.info("Zenoss registration successful. It is located in #{new_resource.devicePath}")
+	  node.set_unless['zenoss-client']['registered'] = true
+	else
+	  Chef::Log.warn("HTTP Status code: " + res.code)
+	  Chef::Log.warn(output)
+	  raise "Zenoss registration failed!"
+	end
+    new_resource.updated_by_last_action(true)
+  end
 end
