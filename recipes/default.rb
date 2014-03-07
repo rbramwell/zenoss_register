bfb6e78fb7bbe94b9a3e256874a7f6b13da526d1
Eric G. Wolfe <eric.wolfe@gmail.com>
2014-03-07 11:36:39 -0500

    Change baseuri in default recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,4 @@
 zenoss_client node['hostname'] do
-  baseuri "http://zen02.marshall.edu:8080/zport/dmd/DeviceLoader"
+  baseuri 'http://zen02.marshall.edu:8080'
   productionValue 'production' 
 end
