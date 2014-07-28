6e074b0bdb98c8b209b2e845c2a53cd8178e81c7
Patrick Moore <moore267@marshall.edu>
2014-07-28 09:36:41 -0400

    added support lines in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
 name             'zenoss_register'
 maintainer       'Patrick Moore'
 maintainer_email 'moore267@marshall.edu'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Auto-registers Zenoss monitored servers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
+version          '0.2.3'
+
+supports 'centos'
+supports 'windows'
+supports 'redhat'
+supports 'fedora'
+supports 'debian'
